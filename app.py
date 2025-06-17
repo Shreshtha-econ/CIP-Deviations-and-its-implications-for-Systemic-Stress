3790153a4e84fe10f26e45ab32f58b975cb3639f
Shreshtha-econ <shreshtha453@gmail.com>
2025-06-17 11:27:25 +0200

    Update app.py to use environment PORT for Render deployment

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ import os
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8056))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, port=port, host='0.0.0.0')
